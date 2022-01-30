1c935b04dfd0c4979213656b29679942115f3e90
pve_jjorge <pve_jjorge@uolinc.com>
2022-01-30 12:50:00 -0300

    api specification

---
+++ src/main/java/com/learn/packt/modernapi/modernapi/ModernapiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.packt.modernapi.modernapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/packt/modernapi/modernapi/ModernapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.packt.modernapi.modernapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
